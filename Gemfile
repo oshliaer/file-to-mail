2ef8187488a00571e7d20522f0496fb1bb1e4d67
Alexander Ivanov <oshli.a.er@gmail.com>
2026-09-07 14:33:37 +0300

    feat: add send mail
    Signed-off-by: Alexander Ivanov <oshli.a.er@gmail.com>

--- Gemfile
+++ Gemfile
@@ -5,3 +5,9 @@ gem 'mail'
 gem 'rackup'
 gem 'puma'
 gem 'dotenv'
+
+group :development do
+  # Форматирование (.rubocop.yml, автоматически при сохранении)
+  # и линт (.rubocop_lint.yml, вручную через bin/lint).
+  gem 'rubocop', require: false
+end
--- app.rb
+++ app.rb
@@ -10,13 +10,17 @@ set :bind, ENV['APP_ADDRESS'] || '0.0.0.0'
 set :port, ENV['APP_PORT'] ? ENV['APP_PORT'].to_i : 8080
 
 SMTP_OPTIONS = {
-  address: ENV['SMTP_ADDRESS'],
-  port: ENV['SMTP_PORT'].to_i,
+  address: ENV['SMTP_ADDRESS'], # 'smtp.mail.ru'
+  port: ENV['SMTP_PORT'].to_i, # 465
   user_name: ENV['SMTP_USER_NAME'],
   password: ENV['SMTP_PASSWORD'],
-  authentication: 'plain',
-  enable_ssl: true,
-  enable_starttls_auto: true
+  authentication: :plain, # В Ruby лучше использовать символ :plain, а не строку 'plain'
+
+  # Вот эта связка лечит таймаут на Mail.ru:
+  ssl: true,                    # Включаем жесткий SSL для порта 465
+  tls: true,                    # Дублируем для совместимости с net/smtp
+  enable_starttls: false, # Намертво КАТЕГОРИЧЕСКИ ОТКЛЮЧАЕМ starttls
+  enable_starttls_auto: false # Отключаем авто-попытки включить starttls
 }
 
 get '/' do
@@ -52,14 +56,26 @@ end
 post '/send_email' do
   return 'Missing parameters. Please provide both "to" and "file".' unless params[:to] && params[:file]
 
-  params[:to]
-  params[:subject] || 'No Subject'
-  params[:file][:tempfile]
-  params[:file][:filename]
-
   Mail.defaults do
     delivery_method :smtp, SMTP_OPTIONS
   end
 
-  'OK'
+  to_address = params[:to]
+  subject = params[:subject] || 'No Subject'
+  body = params[:body] || 'Please find the attached file.'
+  file_name = params[:file][:filename]
+  file_tmp = params[:file][:tempfile]
+
+  begin
+    Mail.deliver do
+      from SMTP_OPTIONS[:user_name]
+      to to_address
+      subject subject
+      body body
+      add_file filename: file_name, content: file_tmp.read
+    end
+  rescue StandardError => e
+    status 500
+    "<h3>Ошибка отправки:</h3><pre style='color:red;'>#{e.message}</pre><a href='/'>Назад</a>"
+  end
 end
